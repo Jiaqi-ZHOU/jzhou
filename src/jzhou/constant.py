56eea9bd1c8161d52657ff71bf41534cdb7dd4cb
Jiaqi-ZHOU <jiaqi.zhou@uclouvain.be>
2024-11-03 13:22:21 +0100

    Change class name

--- src/jzhou/constant.py
+++ src/jzhou/constant.py
@@ -1,15 +1,21 @@
-class MyClass:
-    def __init__(self, **kwargs):
+"""Module for constants."""
+
+
+class DotDict:
+    """Attribute dictionary, access property by dot."""
+
+    def __init__(self, **kwargs):  # noqa: D107
+        # """Initialize."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
 
-fontsizes = MyClass(label=14, tick=12)
+fontsizes = DotDict(label=14, tick=12)
 
 
-colors = MyClass(blue="royalblue", green="limegreen", red="tomato", grey="grey")
+colors = DotDict(blue="royalblue", green="limegreen", red="tomato", grey="grey")
 
-linestyles = MyClass(
+linestyles = DotDict(
     solid="solid",
     dotted="dotted",
     dashed="dashed",
